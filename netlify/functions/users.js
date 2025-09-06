2111fe6484a61efe45b49d01df53d00537f873d3
sbl-igor <shokeator98@gmail.com>
2025-09-07 04:50:56 +1000

    new users.js4

--- netlify/functions/users.js
+++ netlify/functions/users.js
@@ -1,41 +1,14 @@
 import { google } from "googleapis";
+import serviceAccount from "../../userscashflow.json" assert { type: "json" };
 
 const SCOPES = ["https://www.googleapis.com/auth/spreadsheets"];
 const SPREADSHEET_ID = "16V78B6_ucC19jer1MA9An0RGU2XXqiwkeiZRWKnV4bI";
 const SHEET_NAME = "Users";
 
-// 🔑 Прямо вшитые ключи
 const auth = new google.auth.GoogleAuth({
   credentials: {
-    client_email: "assistuserscashflow@caslhflowusers.iam.gserviceaccount.com",
-    private_key: `-----BEGIN PRIVATE KEY-----
-MIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQDLJPPXGUDInJs/
-YMLrL/o9zmKoSZziRjywbK33IDer/QcQccipXZs6aFjPnp6E+VhTuXvHbjdl3/3U
-1EvdGA5NIMLGoIEHpWGprHAP2CeMQfz6X4akJhaNQQSG3iWptlgk1AXeIZ97RbQg
-+Pg1RLXj91UcJkFDK+jo51vLaeqasb4CG4HlTSQB0XKOmpQMVKGHzLoe3CBMY002
-+uyBd2aFLL+KF5moZztdHkp58lLEP0lIciRyXidNlj3lBrw2TnCMRfLu8Semd7U9
-qdRipqj9ud0Iteq3F13fbcEVrkzty910GtiwAXmcfS8b+ptAIFcCkO5rimblFRct
-0qRGm7dpAgMBAAECggEAAYnqs0D8obhhSv+FjR4B1X4uzEsIc5n9bjGt4f6+D+EH
-W2tyjhPd2o6n849Q8JK/RZ0UpVv98pkvKpxJZH1A6Hp8dzWH3Ky28BUqC84C7gKh
-nazjunJpE9H/rIeEvgPEqFIXJvRWvqqLv38wnsisibOZ0wWC04qC2wwbliTGfeb3
-dX4wNIjJ/ZwDkJtBxo8+xKSulR+SwuxSCgLLI3+0Jhvc1M43thfoZOOw/FrrE54F
-aWMDpY9AV0QHl3sPaNMDiS/pHxe6Wp6/70gsUjfF0ZqcOoIbPbSLLHi6qh/8cOVt
-ZuHY7x/sHexYsWlXx5qSkUsHl792V6r9gmwCY9MaoQKBgQD6wHn5aPWi5s9AIT1S
-Rvsowt4BInUIM/K5j4pQn3dfhynUxXaUYX90Drgbo5fTYGdHvNIYARfAT5ROtsRz
-1Jn8bYC4WEo7zkSYiN8EU6+2ynScGJoRXMoO9j2SH+ls/PAxHWAfbV+rMwRzrGTm
-BfKErKnE/ImE3vBWmmE6IjdGWQKBgQDPZWVaN1PWrWu9IVkbMDd7Ub5Cee/xHor7
-kVntV0X09B8NqaieUjt05kU+J8EG/+uWagOlhemV3mbpi7GDygs/oWXhgBUBwQaa
-6AxhwXnT93yE/dweNZVwbFqdeumrh41ZPPHpcwJ1678mkU0ncDs6MEsCAvrR4mOT
-XnISADX3kQKBgQD4YyewRydjuc080nBjC0sKIpY1e4OAn6zl4Wl62awvJb3rNbtz
-2mydc2MuhcwEp+9G4aUgAB0d8/aOTn+l2ci7g2FJurO2MGpEyvhX2eoqYLxIUBp1
-7nt9fLCbCh95RclMbbPlDbwB42SajP/Z17RXdo4pQpua0hgzQXPkWTZ16QKBgHnL
-awzjBwpAh8/nAKQ12a9DHv99CorITAhWoMAAC6ZBoubsJojrc90/yZQp1nLc4iZQ
-Fdoj3TxVAozOq63gfmCc0O7vv+ZqzA02jubtrzTq1yZmyti20Q7XoxNudkF0dm7c
-mphlISHNE1F77RpnD3EpoTMKOu/9CksmIMkvdIHxAoGAI7yeEHpsxLfGeYksUVh8
-0cuv8wFDvZ2f8+BgpR6zq4Ml7xI6BcwztyQlymgEXErBT9DtGnC1p1jqWuHfJxDS
-LWP48KG60Jsipxzarro+n+0e0yLwrDpEtBwXjGgGCh0bloRBb10P+qSCMfEsspSb
-r5cykw8hEZhNOVGXQpZQYg8=
------END PRIVATE KEY-----`,
+    client_email: serviceAccount.client_email,
+    private_key: serviceAccount.private_key,
   },
   scopes: SCOPES,
 });
